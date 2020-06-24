67689e2fc8e23412277b0b6d0534dbc1eee4a132
Suraj Singh <50519406+surajsly@users.noreply.github.com>
2020-06-24 14:58:11 +0530

    button added

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,21 @@
 import React from "react";
 
 class App extends React.Component {
+  state = { resource: "posts" };
   render() {
-    return <div>App</div>;
+    return (
+      <div>
+        <div>
+          <button onClick={() => this.setState({ resource: "posts" })}>
+            Posts
+          </button>
+          <button onClick={() => this.setState({ resource: "todos" })}>
+            todos
+          </button>
+        </div>
+        {this.state.resource}
+      </div>
+    );
   }
 }
 
