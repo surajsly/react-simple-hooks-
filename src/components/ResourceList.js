64b5f8cec9e6af2b8b3ae8d8a1070793d1cb9350
Suraj Singh <50519406+surajsly@users.noreply.github.com>
2020-06-24 18:07:52 +0530

    hooks useeffect

---
+++ src/components/ResourceList.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const useResources = (resource) => {
+  const [resources, setResources] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const response = await axios.get(
+        `http://jsonplaceholder.typicode.com/${resource}`
+      );
+
+      setResources(response.data);
+    })(resource);
+  }, [resource]);
+
+  return resources;
+};
+
+const ResourceList = ({ resource }) => {
+  var resources = useResources(resource);
+
+  return (
+    <ul>
+      {resources.map((record) => (
+        <li key={record.id}>{record.title}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default ResourceList;
