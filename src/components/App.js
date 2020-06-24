c37d0d326c09c3b6e8f3cf91c27384b23e1b76cb
Suraj Singh <50519406+surajsly@users.noreply.github.com>
2020-06-24 15:06:31 +0530

    use useState

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
-class App extends React.Component {
-  state = { resource: "posts" };
-  render() {
-    return (
+const App = () => {
+  const [resource, setResource] = useState("posts");
+
+  return (
+    <div>
       <div>
-        <div>
-          <button onClick={() => this.setState({ resource: "posts" })}>
-            Posts
-          </button>
-          <button onClick={() => this.setState({ resource: "todos" })}>
-            todos
-          </button>
-        </div>
-        {this.state.resource}
+        <button onClick={() => setResource("posts")}>Posts</button>
+        <button onClick={() => setResource("todos")}>todos</button>
       </div>
-    );
-  }
-}
+      {resource}
+    </div>
+  );
+};
 
 export default App;
